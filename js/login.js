2ad13badc32d4c4a26246248093d81f2c2a80f53
Chrissebah <christian.grotan@gmail.com>
2023-10-04 14:01:09 +0200

    Changed login.js

--- js/login.js
+++ js/login.js
@@ -5,30 +5,55 @@ const loginForm = document.getElementById('loginForm');
 loginForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const username = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     try {
-        // Check for credentials in localStorage
-        const storedUserData = localStorage.getItem(username);
-        
-        if (storedUserData) {
-            const userData = JSON.parse(storedUserData);
-            
-            if (userData.password === password) {
-                // Password matches, user is authenticated
-                alert('Login successful!');
-                // Redirect to the protected content page
-                window.location.href = 'dashboard.html';
-            } else {
-                // Password does not match
-                alert('Login failed. Please check your credentials.');
-            }
+        // Object for user inputted values
+        const userData = {
+            email: email,
+            password: password,
+        };
+
+        // Console log to help diagnose the issue
+        console.log('Sending POST request to:', 'https://api.noroff.dev/api/v1/social/auth/login');
+        console.log('Request method:', 'POST');
+
+        // Make a POST request to the login endpoint
+        const response = await fetch('https://api.noroff.dev/api/v1/social/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        });
+
+        if (response.ok) {
+            // Login successful
+            const data = await response.json();
+            // Store user data in localStorage or use it as needed
+            const username = data.name;
+            const userEmail = data.email;
+            const userAccessToken = data.accessToken;
+
+            // Example: Store user data in localStorage
+            const userObject = {
+                name: username,
+                email: userEmail,
+                accessToken: userAccessToken,
+            };
+
+            localStorage.setItem(username, JSON.stringify(userObject));
+
+            // Redirect to the protected content page
+            window.location.href = 'dashboard.html';
         } else {
-            // User not found in localStorage
-            alert('Login failed. User not found.');
+            // Login failed, handle the error
+            console.error('Login failed');
+            alert('Login failed. Please check your credentials.');
         }
     } catch (error) {
         console.error('An error occurred:', error);
+        alert('An error occurred. Please try again later.');
     }
 });
